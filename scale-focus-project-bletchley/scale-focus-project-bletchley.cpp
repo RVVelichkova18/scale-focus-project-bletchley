16bec369bf55444b4f875a7d7745d27c167ac6f9
kvbolashikova18 <59692632+kvbolashikova18@users.noreply.github.com>
2020-12-13 17:46:07 +0200

    Add function "randomInt"

--- scale-focus-project-bletchley/scale-focus-project-bletchley.cpp
+++ scale-focus-project-bletchley/scale-focus-project-bletchley.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <time.h>   
 using namespace std;
 
-int main() {
-   
+int randomInt() {
+    return rand() % (7 - 0 + 1) + 0;
 }
 
+
+int main() {
+    srand(time(NULL));
+}
