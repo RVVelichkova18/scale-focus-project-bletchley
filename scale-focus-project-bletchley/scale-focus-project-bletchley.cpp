d7b2ba7584c59f75f6c6d5b70632fa4ddce24768
kvbolashikova18 <59692632+kvbolashikova18@users.noreply.github.com>
2020-12-13 17:47:10 +0200

    Add function "checkForSameNumberAndPosition"

--- scale-focus-project-bletchley/scale-focus-project-bletchley.cpp
+++ scale-focus-project-bletchley/scale-focus-project-bletchley.cpp
@@ -6,6 +6,18 @@ int randomInt() {
     return rand() % (7 - 0 + 1) + 0;
 }
 
+int checkForSameNumberAndPosition(int* code, int* userGuess) {
+    int count = 0;
+
+    for (int i = 0; i < 4; i++) {
+        if (code[i] == userGuess[i]) {
+            count++;
+            userGuess[i] = userGuess[i] * (-1);
+        }
+    }
+
+    return count;
+}
 
 int main() {
     srand(time(NULL));
